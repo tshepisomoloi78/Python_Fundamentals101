6c8afdd9fedaba3b08df584b78a40c3d95a5c491
wtc <wtc@pop-os.localdomain>
2024-09-13 15:52:44 +0200

    saving changes

--- basic_syntax.py
+++ basic_syntax.py
@@ -6,11 +6,16 @@ def assign_variables():
         tuple: A tuple containing the values of variables x, y, z, a, and b.
     """
     # Assign the integer 10 to the variable x.
+    x = 10
     # Assign the float 20.5 to the variable y.
+    y = 20.5
     # Assign the string 'Hello, World!' to the variable z.
+    z = 'Hello, World!'
     # Assign the boolean True to the variable a.
+    a = True
     # Assign the boolean False to the variable b.
-    return # Return the values of x, y, z, a, and b as a tuple.
+    b = False
+    return (x,y,z,a,b) # Return the values of x, y, z, a, and b as a tuple.
 
 def get_variable_type(variable):
     """
@@ -23,7 +28,7 @@ def get_variable_type(variable):
         str: A string representing the data type of the input variable.
     """
     variable_type = type(variable).__name__  # Get the data type of the input variable.
-    return # Return the data type as a string.
+    return variable_type # Return the data type as a string.
 
 def get_variable_types():
     """
@@ -32,13 +37,17 @@ def get_variable_types():
     Returns:
         tuple: A tuple containing the data types of variables x, y, z, a, and b.
     """
-    x, y, z, a, b = assign_variables # Assign values to x, y, z, a, and b using the assign_variables function.
+    x, y, z, a, b = assign_variables() # Assign values to x, y, z, a, and b using the assign_variables function.
     type_x = get_variable_type(x)  # Get the data type of x using the get_variable_type function.
     # Get the data type of y using the get_variable_type function.
+    type_y = get_variable_type(y)
     # Get the data type of z using the get_variable_type function.
+    type_z = get_variable_type(z)
     # Get the data type of a using the get_variable_type function.
+    type_a = get_variable_type(a)
     # Get the data type of b using the get_variable_type function.
-    return # Return the data types as a tuple.
+    type_b = get_variable_type(b)
+    return (type_x,type_y,type_z,type_a,type_b) # Return the data types as a tuple.
 
 def arithmetic_operations():
     """
@@ -47,13 +56,13 @@ def arithmetic_operations():
     Returns:
         tuple: A tuple containing the results of addition, subtraction, multiplication, division, and modulus operations.
     """
-    x, y, z, a, b = assign_variables # Assign values to x, y, z, a, and b using the assign_variables function.
-    sum_result # Calculate the sum of x and y.
-    difference_result  # Calculate the difference between x and y.
-    product_result # Calculate the product of x and y.
-    division_result  # Calculate the division of x by y.
-    modulus_result  # Calculate the modulus of x and y.
-    return  # Return the results as a tuple.
+    x, y, z, a, b = assign_variables() # Assign values to x, y, z, a, and b using the assign_variables function.
+    sum_result = x+y # Calculate the sum of x and y.
+    difference_result = x-y  # Calculate the difference between x and y.
+    product_result = x*y # Calculate the product of x and y.
+    division_result = x/y  # Calculate the division of x by y.
+    modulus_result = x%y  # Calculate the modulus of x and y.
+    return (sum_result,difference_result,product_result,division_result,modulus_result)  # Return the results as a tuple.
 
 def get_numbers():
     """
--- basic_syntax_exercise.py
+++ basic_syntax_exercise.py
@@ -3,35 +3,35 @@
 # 1. Variables: You can store values in variables.
 
 # On the next line store the number '10' in the variable 'x'.
-
+x = 10
 
 # On the next line, store the number '20.5' in the variable 'y'.
-
+y = 20.5
 
 # On the next line, store the string 'Hello, World!' in the variable 'z'.
-
+z = "'Hello, World!'"
 
 # On the next line, store the boolean 'True' in the variable 'a'.
-
+a = True
 
 # On the next line, store the boolean 'False' in the variable 'b'.
-
+b = False
 
 # Use the print statement and the variable 'x' to display the message:
     # "10, is an integer variable."
-print()    
+print(str(x)+ ", is a integer variable.")    
 # Use the print statement and the variable 'y' to display the message: 
     # "20.5, is a float variable."
-print()
+print(str(y)+ ",is a float variable.")
 # Use the print statement and the variable 'z' to display the message: 
     # " 'Hello, World!' is a string variable."
-print()
+print(str(z)+ ",is a string variable")
 # Use the print statement and the variable 'a' to display the message:
     # " 'True', is a boolean variable."
-print()
+print(str(a)+ ",is a boolean.")
 # Use the print statement and the variable 'b' to display the message:
     # " 'False', is a boolean variable.
-print()    
+print(str(b)+ ", is a boolean.")    
 
 
 # 2. Data Types: Python has several built-in data types.
@@ -42,23 +42,23 @@ print()
 
 # Edit the code below to print:
     # "10 belongs to <class 'int'>: meaning it is an integer."
-print()
+print(type(x))
 
 # Edit the code below to print:
     # " 20.5 belongs to <class 'float'>: meaning it is a float."
-print()
+print(type(y))
 
 # Edit the code below to print:
     # " 'Hello, World!', belongs to <class 'str'>: meaning it is a string."
-print()
+print(type(z))
 
 # Edit the code below to print:
     # "'True', belongs to <class 'bool'>: meaning it is a boolean."
-print()
+print(type(a))
 
 # Edit the code below to print:
     # "'False', belongs to <class 'bool'>: meaning it is a boolean.
-print()
+print(type(b))
 
 
 # 3. Basic Operators: 
@@ -66,19 +66,24 @@ print()
 # Arithmetic Operators
 
 # Addition: print the sum of 'x' and 'y'.
-print("The sum of 'x' and 'y' is: ")
+sum = x+y
+print("The sum of 'x' and 'y' is: "+str(sum))
 
 # Subtraction: print the difference between 'x' and 'y'.
-print("The difference between 'x' and 'y' is: ")
+difference = x-y
+print("The difference between 'x' and 'y' is: "+str(difference))
 
 # Multiplication: print the product of 'x' and 'y'.
-print("The product of 'x' and 'y' is: ")
+multiplication = x*y
+print("The product of 'x' and 'y' is: "+str(multiplication))
 
 # Division: print the answer when you divide 'x' by 'y'.
-print("The division of 'x' by 'y' is:")
+division = x/y
+print("The division of 'x' by 'y' is:"+str(division))
 
 # Modulus (remainder of the division): print the remainder after dividing 'x' by 'y'.
-print("The modulus of 'x' and 'y' is: ")
+modulus = x%y
+print("The modulus of 'x' and 'y' is: "+str(modulus))
 
 
 # 4. Indentation: 
@@ -88,4 +93,5 @@ print("The modulus of 'x' and 'y' is: ")
 
 # for number in range(6):
 # print()  # This line is part of the for loop and it must be indented.
-    
+for number in range(6):
+    print(number)
